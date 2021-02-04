15f40c4019a1ffca908deb374fc8a0af15d73844
Andrea Lopez Flores <andreea.lop@gmail.com>
2021-02-04 15:27:53 -0600

    Update index.js

--- NodeJS/src/index.js
+++ NodeJS/src/index.js
@@ -35,7 +35,7 @@ app.listen( 3001, () => {
 app.use(function (req, res, next) {
 
         // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://104.197.230.46:3000');
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
